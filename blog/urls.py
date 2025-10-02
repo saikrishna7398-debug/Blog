3a22a04d7d91ae8d96f57f1f096593379cb6ea04
T.Saikrishna Rao <thandekarsaikrishna@gmai.com>
2025-10-02 12:39:09 +0530

    Configured urls.py for media & static

--- blog/urls.py
+++ blog/urls.py
@@ -46,4 +46,8 @@ urlpatterns = [
     path('', include('b.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
